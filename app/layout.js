7a0b835d00711f4c5ff6ba138bde4e9be3178658
sguzmanr <sguzmanr2@gmail.com>
2025-09-21 08:09:53 -0500

    Removed alert

--- app/layout.js
+++ app/layout.js
@@ -53,12 +53,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <div className="w-screen h-screen fixed top-0 left-0 bg-red-500/80 z-50">
-        <div className="w-full h-full flex flex-col gap-2 items-center justify-center">
-          <h1 className="font-extrabold text-white text-4xl text-center">PÁGINA WEB DESHABILITADA</h1>
-          <p className="font-base text-white text-xl text-center">Debido a demoras en pagos, esta pagina web ha sido deshabilitada</p>
-        </div>
-      </div>
       <body className={`antialiased`}>
         {children}
       </body>
